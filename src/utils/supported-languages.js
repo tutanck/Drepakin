1d5da718f8cfc54c5180a8c685ff4c6d9e57a0d5
tutanck <tutanck@gmail.com>
2020-06-05 21:51:55 +0200

    select language from url

--- src/utils/supported-languages.js
+++ src/utils/supported-languages.js
@@ -1,8 +1,26 @@
-export default {
+import { loadPreferredLanguage } from './preferred-language';
+
+const supportedLanguages = {
   fr_FR: { key: 'fr_FR', local: 'fr-FR', name: 'Français', display: 'Fr' },
   en_US: { key: 'en_US', local: 'en-US', name: 'English', display: 'En' },
 };
 
 const defaultLanguage = 'fr_FR';
 
-export { defaultLanguage };
+const selectedLanguage = (lng) => {
+  if (supportedLanguages[lng]) {
+    return lng;
+  }
+
+  const preferredLanguage = loadPreferredLanguage();
+
+  if (preferredLanguage) {
+    return preferredLanguage;
+  }
+
+  return defaultLanguage;
+};
+
+export default supportedLanguages;
+
+export { selectedLanguage };
